2aeff0e40dcb98b6c1a62a2987e535d90844c499
Md. Shakil Rana <159621858+MDshakilr@users.noreply.github.com>
2024-05-08 19:07:04 +0600

    Update Binary Search.cpp

--- Searching/Binary Search.cpp
+++ Searching/Binary Search.cpp
@@ -33,3 +33,39 @@ int main()
         cout << "Element not found" << endl;
     return 0;
 }
+
+
+
+/*#include <bits/stdc++.h>
+using namespace std;
+
+int binarySearchRecursive(int arr[], int l, int r, int target)
+{
+    if (l <= r)
+    {
+        int mid = l + (r - l) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+
+        if (arr[mid] < target)
+            return binarySearchRecursive(arr, mid + 1, r, target);
+        else
+            return binarySearchRecursive(arr, l, mid - 1, target);
+    }
+    return -1;
+}
+
+int main()
+{
+    int arr[] = {5, 7, 9, 12, 15, 20, 24, 27, 33, 40, 50, 65, 72};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 33;
+    int index = binarySearchRecursive(arr, 0, size - 1, target);
+    if (index != -1)
+        cout << "Element found at index: " << index << endl;
+    else
+        cout << "Element not found" << endl;
+    return 0;
+}
+*/
